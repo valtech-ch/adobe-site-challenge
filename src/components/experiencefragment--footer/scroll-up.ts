3222d3591aaee2b282929d60e4547f4bfe80d4bd
Ivana <ivana.arsova@codeflair.ch>
2021-04-08 23:00:13 +0200

    scroll up button only if there is something to scroll

--- src/components/experiencefragment--footer/scroll-up.ts
+++ src/components/experiencefragment--footer/scroll-up.ts
@@ -4,4 +4,13 @@ window.addEventListener('DOMContentLoaded', () => {
     buttonScroll.addEventListener('click', (e) => {
         window.scrollTo({top: 0, behavior: 'smooth'});
     })
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+            buttonScroll.style.display = "block";
+        } else {
+            buttonScroll.style.display = "none";
+        }
+    }
 })
